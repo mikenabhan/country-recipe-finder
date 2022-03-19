2bd49b60d48907b811b953d05b6f4cfff18c89b5
Mike Nabhan <13139061+mikenabhan@users.noreply.github.com>
2022-03-18 21:32:40 -0600

    convert to flask app

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import json
-# import sqlite3
-import os
 import random
+from flask import Flask
+
+app = Flask(__name__)
 
 all_countries = []
+
+@app.route('/')
 
 def get_county():
     with open('./all_countries.json', "r") as country_file:
         all_countries = json.load(country_file)
         random_country = random.choice(all_countries)
-    return random_country['Name']
-print(get_county())
+    return random_country
+
